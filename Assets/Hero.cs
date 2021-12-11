5b891e34c2732d81dc93682e76147cbc787003a5
Daniel Pietrzeniuk <ProgramistycznySwir@gmail.com>
2021-12-11 19:58:04 +0100

    Implemented basic movement

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Croisant_Crawler.Core;
+using Croisant_Crawler.UnityExtensions;
 
 public class Hero : MonoBehaviour
 {
@@ -21,6 +23,26 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.W))
+            Move(Croisant_Crawler.Data.Vector2Int.Up);
+        else if(Input.GetKeyDown(KeyCode.S))
+            Move(Croisant_Crawler.Data.Vector2Int.Down);
+        else if(Input.GetKeyDown(KeyCode.A))
+            Move(Croisant_Crawler.Data.Vector2Int.Left);
+        else if(Input.GetKeyDown(KeyCode.D))
+            Move(Croisant_Crawler.Data.Vector2Int.Right);
+    }
+
+    public void Move(Croisant_Crawler.Data.Vector2Int dirrection)
+    {
+        var nextPos = stats.position + dirrection;
+        var newRoom = GameMaster.currentFloorData.rooms[stats.position].connections
+                .Where(room => room.position == nextPos)
+                .FirstOrDefault();
+        if(newRoom is null)
+            return;
         
+        stats.position = newRoom.position;
+        transform.position = GameMaster.currentFloor._grid.CellToLocal(stats.position.ToUnityVector3Int());
     }
 }
