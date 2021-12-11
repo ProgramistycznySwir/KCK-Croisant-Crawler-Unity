a567fa11d085cd8351bd2c8a85a566eba7573f8d
DwieStronyMocy <igomidormid@gmail.com>
2021-12-11 23:24:02 +0100

    HP Bar

--- Assets/PlayerStats_Display.cs
+++ Assets/PlayerStats_Display.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Croisant_Crawler.Core;
 
 public class PlayerStats_Display : MonoBehaviour
 {
     public Hero hero;
+    public Image healthBar;
+
 
     public TMPro.TextMeshProUGUI text_HP;
-
-    // Start is called before the first frame update
     void Start()
     {
         hero.stats.HP_OnChange += UpdateHP;
+
     }
 
     public void UpdateHP(Stats stats)
     {
-        text_HP.text = $"HP: {stats.HP}";
+        text_HP.text = $"HP: {stats.HP.value} / {stats.HP.range.max}";
+        healthBar.fillAmount = stats.HP.Percent;
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
+
     }
 }
