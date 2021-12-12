3590d43e2d718261524f516dcc77906230738131
DwieStronyMocy <igomidormid@gmail.com>
2021-12-12 16:13:06 +0100

    XP and Health bars

--- Assets/PlayerStats_Display.cs
+++ Assets/PlayerStats_Display.cs
@@ -8,9 +8,11 @@ public class PlayerStats_Display : MonoBehaviour
 {
     public Hero hero;
     public Image healthBar;
+    public Image expBar;
 
 
     public TMPro.TextMeshProUGUI text_HP;
+    public TMPro.TextMeshProUGUI text_XP;
     void Start()
     {
         hero.stats.HP_OnChange += UpdateHP;
@@ -21,6 +23,9 @@ public class PlayerStats_Display : MonoBehaviour
     {
         text_HP.text = $"HP: {stats.HP.value} / {stats.HP.range.max}";
         healthBar.fillAmount = stats.HP.Percent;
+        //TODO currently XP just a mockup, need to make it properly count XP
+        expBar.fillAmount  = hero.stats.Exp / (PlayerStats.ExpPerLevel / 2) * hero.stats.Lvl * (hero.stats.Lvl - 1);
+        text_XP.text = $"XP: {hero.stats.Exp} / {(PlayerStats.ExpPerLevel / 2) * hero.stats.Lvl * (hero.stats.Lvl - 1)}";
 
     }
 
