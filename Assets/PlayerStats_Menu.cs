52f0cca968ad4826b7eeb01a8f9826fc10afa581
DwieStronyMocy <igomidormid@gmail.com>
2021-12-13 01:26:41 +0100

    More stats updating correctly

--- Assets/PlayerStats_Menu.cs
+++ Assets/PlayerStats_Menu.cs
@@ -20,10 +20,13 @@ public class PlayerStats_Menu : MonoBehaviour
     {
         hero.stats.HP_OnChange += UpdateHP;
         hero.stats.Agi_OnChange += UpdateAgi;
+        hero.stats.Vit_OnChange += UpdateVit;
+        hero.stats.Str_OnChange += UpdateStr;
+        UpdateHP(hero.stats);
         UpdateAgi(hero.stats);
         UpdateVit(hero.stats);
         UpdateStr(hero.stats);
-
+        //TODO update statystyk przy zmianie widoku
         text_XP_stat.text = $"XP: {hero.stats.Exp} / {(PlayerStats.ExpPerLevel / 2) * hero.stats.Lvl * (hero.stats.Lvl - 1)}";
         text_LVL_stat.text = $"LVL: {hero.stats.Lvl}";
         text_SkillPoint_stat.text = $"Skill Points left:    {hero.stats.SkillPoints}";
