6425ac13771ff76156ce8f8b2c262c1f77b8825f
Daniel Pietrzeniuk <ProgramistycznySwir@gmail.com>
2021-12-19 18:59:48 +0100

    Starting to work on Fight system

--- Assets/FightManager.cs
+++ Assets/FightManager.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Croisant_Crawler.Core;
 
 public class FightManager : MonoBehaviour
 {
+    /// Singleton.
+    public static FightManager instance { get; private set; }
+    public FightManager() => instance = this;
+
+    public Fight fight { get; private set; }
+    public Hero hero;
+
+    public GameObject enemyPrefab;
+    public Transform[] enemySockets;
+    public List<Enemy> enemies;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,17 @@ public class FightManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void StartFight(int distanceFromStart)
+    {
+        fight = new Fight(distanceFromStart);
+
+        for (int i = 0; i < fight.enemies.Count; i++)
+            enemies.Add(Object.Instantiate(enemyPrefab, enemySockets[i])
+                    .GetComponent<Enemy>()
+                    .Init(fight.enemies[i]));
         
     }
 }
